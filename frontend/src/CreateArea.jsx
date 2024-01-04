2b10c8b1a107fbbbda06a94fe61f8202d957b80a
ShashankShinde16 <119064985+ShashankShinde16@users.noreply.github.com>
2024-01-04 14:38:46 +0530

    Update CreateArea.jsx
    updation in end-point

--- frontend/src/CreateArea.jsx
+++ frontend/src/CreateArea.jsx
@@ -31,7 +31,7 @@ function CreateArea(props) {
       setAdding(true);
 
       try {
-        const response = await fetch('http://localhost:5000/', {
+        const response = await fetch('https://keeper-app-backend-ai76.onrender.com/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
